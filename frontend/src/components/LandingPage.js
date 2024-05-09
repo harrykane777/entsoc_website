2f368dd2868eba240ecf6e5db47d7e5a946f11b2
nelson <z5417695@unsw.edu.au>
2024-05-09 16:03:44 +1000

    changes

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../styles/LandingPage.css';
 
 const LandingPage = () => {
     return (
-        <div>
+        <div className="main-container">
             <h1>Hello world</h1>
         </div>
     );
