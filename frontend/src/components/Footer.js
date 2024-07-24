4e2bfa70ec87084e0722762d9c53087ec7b2708c
Briannica <134359153+Briannica@users.noreply.github.com>
2024-07-25 02:37:34 +1000

    added footer with functionality

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import '../styles/Footer.css';
+import facebook from '../public/facebook.png'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="footer">
+            <div className="footer-container">
+                <div className="footer-title">Contact Us</div>
+                <a onClick={(e) => {window.location.href ='mailto:unsw.entrepreneurssociety@gmail.com';}}>unsw.entrepreneurssociety@gmail.com</a>
+            </div>
+            <div className="logo">placeholder</div>
+            <div className="footer-container">
+                <div className="footer-title">Follow Our Socials</div>
+                <div className="logo-container">
+                    <a href="https://www.facebook.com/unsw.entsoc/">
+                        <img src={facebook} className="socials-logo"/>
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
